bb97db067d903171918942d1d4fc00249f91d5be
AdePrasTyo <adeprasestyo6@gmail.com>
2024-08-14 11:05:46 +0700

    Dashboard admin
    Belum selesai

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -11,16 +11,47 @@ class DashboardController extends Controller
 {
     public function index()
     {
-        // Menghitung jumlah total pengguna
         $userCount = User::count();
 
-        // Menghitung jumlah pemesanan yang dibuat hari ini
         $todayBookingsCount = Booking::whereDate('created_at', Carbon::today())->count();
 
-        // Mengirimkan data ke view
+        $yearlyIncome = Booking::whereYear('created_at', Carbon::now()->year)->sum('total_price');
+
+        $monthlyIncome = Booking::whereMonth('created_at', Carbon::now()->month)
+            ->whereYear('created_at', Carbon::now()->year)
+            ->sum('total_price');
+
+        $weeklyIncome = Booking::whereBetween('created_at', [Carbon::now()->startOfWeek(), Carbon::now()->endOfWeek()])
+            ->sum('total_price');
+
+        $dailyIncome = Booking::whereDate('created_at', Carbon::today())->sum('total_price');
+
+        $incomeLabels = [];
+        $incomeData = [];
+        for ($i = 0; $i < 12; $i++) {
+            $month = Carbon::now()->subMonths($i);
+            $incomeLabels[] = $month->format('F');
+            $incomeData[] = Booking::whereMonth('created_at', $month->month)
+                ->whereYear('created_at', $month->year)
+                ->sum('total_price');
+        }
+
+        $satisfactionData = [
+            'Kurang Baik' => Booking::where('satisfaction', 'Kurang Baik')->count(),
+            'Baik' => Booking::where('satisfaction', 'Baik')->count(),
+            'Sangat Baik' => Booking::where('satisfaction', 'Sangat Baik')->count(),
+        ];        
+
         return view('admin.menu.dashboard', [
             'userCount' => $userCount,
-            'todayBookingsCount' => $todayBookingsCount
+            'todayBookingsCount' => $todayBookingsCount,
+            'yearlyIncome' => $yearlyIncome,
+            'monthlyIncome' => $monthlyIncome,
+            'weeklyIncome' => $weeklyIncome,
+            'dailyIncome' => $dailyIncome,
+            'incomeLabels' => array_reverse($incomeLabels),
+            'incomeData' => array_reverse($incomeData),
+            'satisfactionData' => array_values($satisfactionData)
         ]);
     }
 }
